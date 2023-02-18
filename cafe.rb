6a3295c5bcc98af1456b28de3666b91bc4867aa1
kiharakeito <kiharakeito@gmail.com>
2023-02-18 20:44:38 +0900

    プログラムの修正後ソースを提出いたします
    入力した注文番号も問題あればメッセージ表示してプログラム終了するよう変更いたしました

--- cafe.rb
+++ cafe.rb
@@ -19,9 +19,17 @@ def take_order(menus)
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
-  order_number = gets.to_i
+  order_number = gets.to_i - 1
+  if !isInRange(order_number, 0, menus.length - 1)
+    puts "注文番号を間違えていますよ！再度やり直してください"
+    exit
+  end
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
+end
+
+def isInRange(order_number, menus_start, menus_end)
+  return order_number >= menus_start && order_number <= menus_end
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
@@ -30,5 +38,5 @@ order1 = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
 
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
